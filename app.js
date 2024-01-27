dc3efacc9e19f365f7d905442d7724d63e950d86
sujal patel <worldonwheels701@gmail.com>
2024-01-26 22:16:56 -0500

    module in node index to app

---
+++ app.js
@@ -0,0 +1,9 @@
+const a=require('./index.js');
+
+console.log(a.name);
+console.log(a.age);
+console.log(a.address);
+
+a.aver(10,20);
+a.sum(10,20);
+a.sub(10,20);
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 console.log("hello world")
-console.log("hello world")
-console.log("hello world")
+
+const a={
+    name:"sachin",
+    age:23,
+    address:"pune",
+
+    aver:(a,b)=>{
+        console.log((a+b)/2)
+    },
+
+    sum:(a,b)=>{
+        console.log(a+b)
+    },
+
+    sub:(a,b)=>{
+        console.log(a-b)
+    },
+}
+
+module.exports=a
